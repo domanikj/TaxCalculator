dd56285c8732d344b2f77246afc1b4074ecef352
Domanik.Johnson@outlook.com <Domanik.Johnson@outlook.com>
2020-03-11 22:59:22 -0400

    started with event handler

--- src/Index.js
+++ src/Index.js
@@ -1,2 +1,13 @@
-let f = arg => arg
-console.log(f())
+//let a = document.getElementById('num-people');
+
+document.getElementById('plus').addEventListener('click', addOne, true);
+let iGetal = 0
+
+function addOne() {
+   
+  iGetal =  iGetal + 1;
+
+document.getElementById('num-people').value = iGetal;
+
+}
+
