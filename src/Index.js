1a4e4f50d780dacc41708c9933901c4ea6e427c5
Domanik.Johnson@outlook.com <Domanik.Johnson@outlook.com>
2020-03-13 21:42:59 -0400

    finished calculate click function and added close function to toggle class

--- src/Index.js
+++ src/Index.js
@@ -4,29 +4,39 @@ let numbCust = 1
 document.getElementById('plus').addEventListener('click', addOne);
 document.getElementById('minus').addEventListener('click', minusOne);
 document.getElementById('mybtn').addEventListener('click', calculateTax);
+document.getElementById('x').addEventListener('click', close);
 
+
+
+/*
+* Function to calculate the tip
+* @return None
+*/
 function calculateTax() {
-
-let totalCost = parseFloat(document.getElementById('total-cost').value);
+let showTotal  = document.getElementById('show-total')
+let totalCost  = parseFloat(document.getElementById('total-cost').value);
 let percentage = parseFloat(document.getElementById('tip-percentage').value);
+let testCss    = document.getElementsByClassName('card')[1]
 
 let total = ((totalCost * percentage) + totalCost).toFixed(2) / numbCust;
 
-console.log(total)
-console.log(totalCost)
-console.log(percentage)
-//let calculated = totalCost.textContent
-
+if(testCss.classList.contains('hide-card')) {
+  testCss.classList.remove('hide-card')
+  showTotal.textContent = total
+}else{
+  showTotal.textContent = total
+}
 }
 
-
+function close() {
+  document.getElementsByClassName('card')[1].classList.add('hide-card');
+}
 /*
 * Function to add one to customer count
 * @return None
 */
 function addOne() {
   numbCust = numbCust + 1;
-
   document.getElementById('num-people').value = numbCust;
 }
 
@@ -37,7 +47,6 @@ function addOne() {
 function minusOne() {
   if(numbCust > 0){
     numbCust = numbCust - 1;
-    
     document.getElementById('num-people').value = numbCust;
   }else{
     document.getElementById('num-people').value = numbCust;
